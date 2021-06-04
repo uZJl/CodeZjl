3cc871c1279e9dbb0c4e8a0f336e5c52aaeda6b5
uZJl <1969643139@qq.com>
2021-06-04 22:27:31 +0800

    0604

--- 0603/src/Main3.java
+++ 0603/src/Main3.java
@@ -2,4 +2,7 @@
  * Created by JiaLe on 2021/6/3 22:35
  */
 public class Main3 {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ lll/src/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by JiaLe on 2021/6/4 14:46
+ */
+public class Main {
+}
