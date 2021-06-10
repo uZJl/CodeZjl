2cec28df1f58a2ba3df5334c6e4d81dbfe2bfa04
uZJl <1969643139@qq.com>
2021-06-10 22:41:02 +0800

    0610

--- 0609/src/Main1.java
+++ 0609/src/Main1.java
@@ -3,6 +3,6 @@
  */
 public class Main1 {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ 0609/src/test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by JiaLe on 2021/6/10 16:38
+ */
+public class test {
+}
