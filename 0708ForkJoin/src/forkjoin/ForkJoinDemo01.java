753305a56de775cd12a3d20c6d15d0587399a26d
uZJl <1969643139@qq.com>
2021-07-09 08:41:55 +0800

    0709

---
+++ 0708ForkJoin/src/forkjoin/ForkJoinDemo01.java
@@ -0,0 +1,14 @@
+package forkjoin;
+
+/**
+ * Created by JiaLe on 2021/7/8 9:25
+ */
+
+//求和计算
+public class ForkJoinDemo01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10000; i++) {
+
+        }
+    }
+}
--- sort/src/shellSort.java
+++ sort/src/shellSort.java
@@ -8,7 +8,7 @@ public class shellSort {
         int[] arr = { 3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48 };
         for (int gap = arr.length / 2; gap > 0; gap /= 2) {
             for (int i = gap; i < arr.length; i++) {
-                for (int j = i; j > gap - 1 ; j -= gap) {
+                for (int j = i; j > gap - 1; j -= gap) {
                     if (arr[j] < arr[j - gap]) {
                         int temp = arr[j];
                         arr[j] = arr[j - gap];
